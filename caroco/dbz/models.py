e8a969863c3d30d6e6e9ae1322fd78eb27b96517
Sergio Oliveira <sergio.oliveira@mandic.net.br>
2018-05-17 19:20:59 -0300

    subindo de season automaticamente, caso necessario cria a season

--- caroco/dbz/models.py
+++ caroco/dbz/models.py
@@ -5,6 +5,8 @@ from django.db import models
 
 # Create your models here.
 from confronto.models import Confronto
+from django.db.models import Q
+
 
 class Season(models.Model):
     def __str__(self):
@@ -18,6 +20,14 @@ class Guerreiro(models.Model):
     nome = models.CharField("Nome", max_length=200)
     season = models.ForeignKey(Season, on_delete=models.CASCADE, related_name='season')
 
+    def exist_lowSeason(self,guerreiro):
+        season = guerreiro.season
+        for s in Season.objects.filter(id__lt=season.id):
+            print len(Guerreiro.objects.filter(season=s))
+            if len(Guerreiro.objects.filter(season=s)) > 0:
+                return True
+        return False
+
 class Partida(models.Model):
     def __str__(self):
         return "%s %d x %d %s" % (self.gz1, self.v1, self.v2,self.gz2)
@@ -28,6 +38,64 @@ class Partida(models.Model):
     v2 = models.IntegerField(default=0)
     confronto = models.ForeignKey(Confronto, on_delete=models.CASCADE)
     pSeason = models.ForeignKey(Season, on_delete=models.CASCADE, related_name='pSeason', default=1)
+
+    def faltaLutar(self,guerreiro):
+        latest_dbz_faltaJ = []
+        allGuerreiros = Guerreiro.objects.filter(season__gte=guerreiro.season)
+        allPartidas = self.allPartidas(guerreiro)
+
+        for g in allGuerreiros:
+            if g == guerreiro:
+                continue
+            inserir=True
+            for p in allPartidas:
+                if p.pSeason != guerreiro.season:
+                    continue
+                if p.gz2 == g:
+                    inserir = False
+                    break
+            if inserir:
+                latest_dbz_faltaJ.append(g)
+        return latest_dbz_faltaJ
+
+    def allPartidas(self, guerreiro):
+        allPartidas = []
+        for p in Partida.objects.filter(Q(gz1=guerreiro.id) | Q(gz2=guerreiro.id)).order_by('-pSeason'):
+            if p.gz1 == guerreiro:
+                allPartidas.append(p)
+                continue
+            else:
+                newp=Partida()
+                newp.gz1=p.gz2
+                newp.v1=p.v2
+                newp.gz2=p.gz1
+                newp.v2=p.v1
+                newp.pSeason = p.pSeason
+                allPartidas.append(newp)
+
+        return allPartidas
+
+    def updateGuerreiroSeason(self, guerreiro):
+        print guerreiro
+        season = guerreiro.season
+        newTemp = season.temporada+1
+        newSeason = Season.objects.filter(temporada=newTemp)
+        if len(newSeason) == None:
+            nS = Season()
+            nS.temporada = newTemp
+            Season.save(nS)
+            newSeason.append(nS)
+        guerreiro.season=newSeason[0]
+        Guerreiro.save(guerreiro)
+
+    def save(self, *args, **kwargs):
+        if not Guerreiro().exist_lowSeason(self.gz1):
+            if len(self.faltaLutar(self.gz1))-1 <= 0:
+                self.updateGuerreiroSeason(self.gz1)
+        if not Guerreiro().exist_lowSeason(self.gz2):
+            if len(self.faltaLutar(self.gz2))-1 <= 0:
+                self.updateGuerreiroSeason(self.gz2)
+        super(Partida, self).save(*args, **kwargs)
 
 class Titulo(models.Model):
     def __str__(self):
--- caroco/dbz/views.py
+++ caroco/dbz/views.py
@@ -88,36 +88,10 @@ def perfil(request,guerreiroId):
 
     template = loader.get_template('dbz/perfil.html')
     latest_dbz_guerreiro = Guerreiro.objects.filter(id=guerreiroId)
-    allPartidas = Partida.objects.filter(Q(gz1=latest_dbz_guerreiro[0].id) | Q(gz2=latest_dbz_guerreiro[0].id)).order_by('-pSeason')
-    latest_dbz_partidas = []
 
-    for p in allPartidas:
-        if p.gz1 == latest_dbz_guerreiro[0]:
-            latest_dbz_partidas.append(p)
-            continue
-        else:
-            newp=Partida()
-            newp.gz1=p.gz2
-            newp.v1=p.v2
-            newp.gz2=p.gz1
-            newp.v2=p.v1
-            newp.pSeason = p.pSeason
-            latest_dbz_partidas.append(newp)
+    latest_dbz_partidas = Partida().allPartidas(latest_dbz_guerreiro[0])
 
-    latest_dbz_faltaJ = []
-    allGuerreiros = Guerreiro.objects.all()
-    for g in allGuerreiros:
-        if g == latest_dbz_guerreiro[0]:
-            continue
-        inserir=True
-        for p in latest_dbz_partidas:
-
-            if p.gz2 == g and p.pSeason == latest_dbz_guerreiro[0].season:
-                print "if "+p.gz2.nome+" == "+g.nome+" and "+str(p.pSeason.temporada)+"== "+str(g.season.temporada)
-                inserir = False
-                break
-        if inserir:
-            latest_dbz_faltaJ.append(g)
+    latest_dbz_faltaJ = Partida().faltaLutar(latest_dbz_guerreiro[0])
 
     rank = createRanking(latest_dbz_guerreiro[0])
     titulo = rank.titulo
