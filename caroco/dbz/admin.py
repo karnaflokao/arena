406d5326d1613c19d139734399cf3ea6496f125c
Sergio Oliveira <sergio.oliveira@mandic.net.br>
2018-05-17 11:48:13 -0300

    add season no admin

--- caroco/dbz/admin.py
+++ caroco/dbz/admin.py
@@ -4,8 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Guerreiro, Partida, Titulo
+from .models import Guerreiro, Partida, Titulo, Season
 
 admin.site.register(Guerreiro)
 admin.site.register(Partida)
 admin.site.register(Titulo)
+admin.site.register(Season)
